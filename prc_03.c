89b405257831dcf2ff2544a2cc0c812765b381f3
Anas Ali Siddiqui <89740466+anasali1671@users.noreply.github.com>
2021-10-09 04:41:59 -0700

    Add files via upload

---
+++ prc_03.c
@@ -0,0 +1,11 @@
+// Q1. Write a c program to calculate the area of a rectangle:
+// a) using hardcoded inputs & 
+// b) using inputs supplied by the user
+//              (a)
+#include<stdio.h>
+int main(){
+    int length=6, breadth=2;
+    int area = length*breadth;
+    printf("The area of this rectangle is %d", area);
+    return 0;
+}
