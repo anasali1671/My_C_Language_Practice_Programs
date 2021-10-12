8c773e717809f2d8e302e1b9184fd2c6cf0eda33
Anas Ali Siddiqui <89740466+anasali1671@users.noreply.github.com>
2021-10-12 10:09:16 -0700

    Add files via upload

---
+++ prc_06.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 9;
+    float b = 6.5;
+    char c = 'b';
+    int d = 65;
+    int e = 65 + 4;
+    printf("value of a is %d \n", a);
+    printf("value of b is %f \n", b);
+    printf("value of c is %c \n", c);
+    printf("Sum of a and d is %d \n", a - d);
+    printf("Sum of a and d is %d \n", e);
+    return 0;
+}
