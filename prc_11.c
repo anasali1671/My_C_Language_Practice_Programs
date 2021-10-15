c1c73d76424f657edf74c53dfa4709c45ec95dd8
Anas Ali Siddiqui <89740466+anasali1671@users.noreply.github.com>
2021-10-15 06:26:32 -0700

    Add files via upload

---
+++ prc_11.c
@@ -0,0 +1,13 @@
+// Equations Solution
+#include <stdio.h>
+
+int main()
+{
+    int x = 3;
+    int y = 4;
+
+    printf("The value of 3*x - 8*y is %d \n", 3*x - 8*y);
+    printf("The value of 8*y / 3*x is %d \n", 8 * y / 3 * x);
+    
+    return 0;
+}
