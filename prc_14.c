928afb49b00924091a6118e5d1f0423b51101216
Anas Ali Siddiqui <89740466+anasali1671@users.noreply.github.com>
2021-10-17 06:20:52 -0700

    Add files via upload

---
+++ prc_14.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 4;
+    int y = 9;
+
+    printf("The value of 3*x - 8*y is %d \n", 4 * x - 9 * y);
+    printf("The value of 8*y / 3*x is %d \n", 4 * y / 9 * x);
+
+    return 0;
+}
